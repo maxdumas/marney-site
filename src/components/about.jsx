ca96eee4fd5546b50f5736c36d49d898b4cbc355
Max Dumas <maltor124@gmail.com>
2020-04-28 23:14:52 -0400

    Reformat contact page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 import portraitImage from "../../assets/portrait.jpg";
+import businessCard from "../../assets/logo2.jpg";
 import resume from "../../assets/resume.pdf";
 import { getAboutData } from "../data";
 
@@ -10,29 +12,42 @@ const About = () => {
   return (
     <div
       style={{
-        margin: "0 64px",
-        columns: 2,
-        columnGap: "32px",
-        columnWidth: "300px",
+        margin: "0 25%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
       }}
     >
-      <div style={{ textAlign: "right" }}>
-        <img src={portraitImage} style={{ height: "300px" }} />
-      </div>
-      <div>
-        <div>
-          <p>{bio}</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <img src={portraitImage} style={{ width: "300px" }} />
+        <div style={{ flexGrow: 1, marginLeft: "8px" }}>
+          <ReactMarkdown>{bio}</ReactMarkdown>
         </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          margin: "32px 0",
+        }}
+      >
+        <img src={businessCard} style={{ width: "300px" }} />
         <div
           style={{
-            marginTop: "16px",
+            flexGrow: 1,
             display: "flex",
-            flexDirection: "row",
             justifyContent: "center",
+            flexDirection: "column",
           }}
         >
-          <a
-            href={`mailto:${email}`}
+          <span
             style={{
               display: "block",
               border: "4px solid white",
@@ -40,8 +55,8 @@ const About = () => {
               padding: "8px",
             }}
           >
-            email
-          </a>
+            {email}
+          </span>
           <a
             href={resume}
             style={{
